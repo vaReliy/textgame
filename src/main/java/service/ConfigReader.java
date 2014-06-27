04345319020a43e9183effa4abdb4f51adc8281d
VG <valeriy@i.ua>
2014-06-28 01:05:53 +0300

    fix config reader

--- src/main/java/service/ConfigReader.java
+++ src/main/java/service/ConfigReader.java
@@ -11,16 +11,25 @@ import java.util.Map;
 
 public class ConfigReader {
     private Map<Integer, String[]> gameConfigValues;
-    private String fileNamePath = "src/main/resources/game_default.src";
+    private boolean isCorrectReadConfig = false;
+    private String fileNamePath = "src/main/resources/game_default.cfg";
 
-    public Map<Integer, String[]> configReadFromFile(String fileName){
+
+    public ConfigReader() {
+        gameConfigValues = configReadFromFile(fileNamePath);
+    }
+
+    public ConfigReader(String fileNamePath) {
+        gameConfigValues = configReadFromFile(fileNamePath);
+    }
+
+    private Map<Integer, String[]> configReadFromFile(String fileName){
         Map<Integer, String[]> config = new HashMap<Integer, String[]>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             String line;
             Integer key;
             while ((line = br.readLine()) != null) {
-//                System.out.println(line);
                 String[] valuesArr = line.split("~");
                 key = Integer.parseInt(valuesArr[0]);
                 String[] values = Arrays.copyOfRange(valuesArr, 1, valuesArr.length);
@@ -28,27 +37,58 @@ public class ConfigReader {
                 config.put(key, values);
             }
             System.out.println("Configuration read!");
-            printConfigAll(config);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            correctStringsEntersInConfig(config);
+//            printConfigAll(config);
+            isCorrectReadConfig = true;
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Wrong filename: " + fnfe);
+        } catch (NumberFormatException nfe) {
+            System.err.println("Wrong values in config data file! Configuration not load!");
+//            nfe.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return config;
     }
 
-    public void printConfigAll(Map<Integer, String[]> gameConfigValues){
+    private void printConfigAll(){
         Iterator<Map.Entry<Integer, String[]>> it = gameConfigValues.entrySet().iterator();
         while (it.hasNext()){
             Map.Entry<Integer, String[]> entry = it.next();
             System.out.println("KEY = " + entry.getKey());
-            System.out.println("VALUES = " + Arrays.toString(entry.getValue()));
+            System.out.println("\tVALUES = " + Arrays.toString(entry.getValue()));
         }
     }
 
-    public static void main(String[] args) {
-        String fileNamePath = "src/main/resources/game_default.src";
-        ConfigReader cr = new ConfigReader();
-        cr.configReadFromFile(fileNamePath);
+    private void correctStringsEntersInConfig(Map<Integer, String[]> gameConfigValues){
+        Iterator<Map.Entry<Integer, String[]>> it = gameConfigValues.entrySet().iterator();
+        while (it.hasNext()){
+            Map.Entry<Integer, String[]> entry = it.next();
+            for (int i = 0; i < entry.getValue().length; i++) {
+                 entry.getValue()[i] = convertEnters(new StringBuilder(entry.getValue()[i]));
+            }
+        }
     }
+
+    private String convertEnters(StringBuilder sb) {
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == 'n' && sb.charAt(i - 1) == '\\') {
+                sb.insert(i, '\n').deleteCharAt(i - 1).deleteCharAt(i);
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public Map<Integer, String[]> getGameConfig(){
+        if (isCorrectReadConfig){
+            return gameConfigValues;
+        }
+        return null;
+    }
+
+    /*public static void main(String[] args) {
+        ConfigReader cr = new ConfigReader();
+        cr.printConfigAll();
+    }*/
 }
