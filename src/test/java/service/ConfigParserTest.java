b34c2aac1e830a7d5a64c4b9a7e1d11d7d84a944
VG <valeriy@i.ua>
2014-06-28 17:53:22 +0300

    fix ConfigParcerTest

--- src/test/java/service/ConfigParserTest.java
+++ src/test/java/service/ConfigParserTest.java
@@ -1,24 +1,29 @@
 package service;
 
 import model.Entity;
-import org.junit.*;
-import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
 import static junit.framework.Assert.assertTrue;
 
 public class ConfigParserTest {
-    private Set<Entity> entitySet;
+    private Map<Integer, Entity> entityMap;
     private ConfigParser cp;
 
 
     @Before
     public void setUp(){
         cp = new ConfigParser();
-        entitySet = cp.getGameContent();
+        entityMap = cp.getEntityMap();
+        System.out.println("size: " + entityMap.size());
     }
 
     @Test
     public void testGetGameContent() throws Exception {
         assertTrue(cp.isCreatedContent());
         cp.printContent();
+
     }
 }
